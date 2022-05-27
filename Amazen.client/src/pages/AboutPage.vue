16c87cba9f20c779abac8ee8421be2eb3c443156
Mark Ohnsman <Mark.Ohnsman@gmail.com>
2022-05-27 15:22:02 -0600

    AboutPage

--- Amazen.client/src/pages/AboutPage.vue
+++ Amazen.client/src/pages/AboutPage.vue
@@ -1,6 +1,25 @@
 <template>
-  <div class="about">
-    <h1>This is the about page</h1>
+  <div class="about container">
+    <div class="row mt-5">
+      <div class="col">
+        <div class="my-3">
+          <h1>Breathe Deep and Relax</h1>
+        </div>
+        <p><b>"I will meditate and then destroy you. Now you've realized the inner mysteries of yoga!"</b></p>
+        <p>Meet yourself where you are equanimity. Without enlightenment, you are lost in darkness! Seek the light!
+          Exhale goddess, stillness, bandha meet yourself. Downward-facing dog upward-facing dog inhale namaste rinse on
+          and off the mats Om, exhale inhale renew. </p>
+        <p>Size is no consequence. Yoga mastery is an ancient discipline of mind and body. I'm your master. Pain is a
+          state of mind and I don't mind your pain. Exhale inhale renew meditation in motion equanimity? Expand out
+          exhale. Shed your ego and become a part of what is around you. Awaken!. Modification exhale, goddess, on and
+          off the mats alignment goddess buti inhale create space flow. </p>
+      </div>
+      <div class="col mt-5 text-center d-flex flex-column justify-content-center">
+        <img src="https://orenbrimer.com/wp-content/uploads/2014/03/StreetFighter_Dahlsim-480x270.jpg" alt="Dahlsim">
+        <p class="m-0"><small><em>"I'm the guy right now"</em></small></p>
+        <p class="ms-5"><small>-Our Founder</small></p>
+      </div>
+    </div>
   </div>
 </template>
 
